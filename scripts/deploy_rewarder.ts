89b50180e6a0f0160b368a1f020e0cefd3b53f23
T-Ph525 <att.pham2017@gmail.com>
2022-06-11 12:02:19 +1000

    script

--- scripts/deploy_rewarder.ts
+++ scripts/deploy_rewarder.ts
@@ -5,20 +5,20 @@ const wmatic = "0x0d500B1d8E8eF31E21C99d1Db9A6444d3ADf1270"
 async function main() {
 
     // Edit these before deployment
-    const rewardToken = wmatic 
-    const rewardPerSecond = 0
+    const rewardToken = "0x4beAf010A2cF3469D26D6754D65Fc5a19e8F3AA0"
+    const rewardPerBlock = 0
     // end of edit
 
     const masterchefv2 = "0xFfDCb4e461130889908444221a8714bbF04D18cA"
     const Rewarder = await ethers.getContractFactory("ComplexRewarder");
-    const rewarder = await Rewarder.deploy(rewardToken, rewardPerSecond, masterchefv2);
+    const rewarder = await Rewarder.deploy(rewardToken, rewardPerBlock, masterchefv2);
     await rewarder.deployed()
   
     console.log("rewarder deployed to:", rewarder.address);
 
     await run("verify:verify", {
         address: rewarder.address,
-        constructorArguments: [rewardToken, rewardPerSecond, masterchefv2],
+        constructorArguments: [rewardToken, rewardPerBlock, masterchefv2],
     })
   }
   
