fdd90277745b63df227fd9d238b1aec5d55b5bfd
Gonzalo Daniel Aguilar <aguilar.daniel.gonzalo@gmail.com>
2023-02-07 22:54:16 -0300

    HardHat TypeScript Config

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -10,49 +10,50 @@ import "solidity-coverage"
 import "hardhat-spdx-license-identifier"
 import { HardhatUserConfig } from "hardhat/types"
 import "hardhat-gas-reporter"
+import { clear } from "console"
 
 const MASTER_PID = 25
 const MCV1 = "0xC5C24B76de65808eD1c17E411c6C5cfC78FA1A98"
 const MCV2 = "0xb80d90DA1231C84DD1327CcaFD9b750e03a0264E"
 
 task("addpool", "Adds pool to MCv2").addParam("allocPoint", "Amount of points to allocate to the new pool", undefined, types.int).addParam("lpToken", "Address of the LP tokens for the farm").addOptionalParam("update", "true if massUpdateAllPools should be called", false, types.boolean).addParam("sleep", "Time in seconds to sleep between adding and setting up the pool", undefined, types.int).setAction(async (taskArgs, hre) => {
-    const wait = (milliseconds) => {
-      return new Promise(resolve => setTimeout(resolve, milliseconds))
-    }
+  const wait = (milliseconds) => {
+    return new Promise(resolve => setTimeout(resolve, milliseconds))
+  }
 
-    let allocPoint, lpToken, tx
-    allocPoint = hre.ethers.utils.parseUnits((taskArgs.allocPoint).toString(), 0)
+  let allocPoint, lpToken, tx
+  allocPoint = hre.ethers.utils.parseUnits((taskArgs.allocPoint).toString(), 0)
 
-    try {
-        lpToken = hre.ethers.utils.getAddress(taskArgs.lpToken)
-    } catch {
-        console.log("ERROR: LP token address not valid")
-        return
-    }
+  try {
+    lpToken = hre.ethers.utils.getAddress(taskArgs.lpToken)
+  } catch {
+    console.log("ERROR: LP token address not valid")
+    return
+  }
 
-    //set this manually here when needed
-    let rewarders = []
-    let overwrite = true
+  //set this manually here when needed
+  let rewarders = []
+  let overwrite = true
 
-    let MCv1 = await hre.ethers.getContractAt("MasterChef", MCV1)
-    let MCv2 = await hre.ethers.getContractAt("MasterChefV2", MCV2)
+  let MCv1 = await hre.ethers.getContractAt("MasterChef", MCV1)
+  let MCv2 = await hre.ethers.getContractAt("MasterChefV2", MCV2)
 
-    console.log("Adding pool...")
-    tx = await MCv2.add(0, lpToken, rewarders, taskArgs.update)
-    await tx.wait();
+  console.log("Adding pool...")
+  tx = await MCv2.add(0, lpToken, rewarders, taskArgs.update)
+  await tx.wait();
 
-    console.log("Sleeping for " + taskArgs.sleep + " seconds...")
-    await wait(taskArgs.sleep * 1000)
+  console.log("Sleeping for " + taskArgs.sleep + " seconds...")
+  await wait(taskArgs.sleep * 1000)
 
-    console.log("Adjusting MCv1 allocation...")
-    let newAlloc = Number(hre.ethers.utils.formatUnits((await MCv1.poolInfo(MASTER_PID)).allocPoint, 0)) + Number(taskArgs.allocPoint)
-    tx = await MCv1.set(MASTER_PID, newAlloc)
-    await tx.wait();
+  console.log("Adjusting MCv1 allocation...")
+  let newAlloc = Number(hre.ethers.utils.formatUnits((await MCv1.poolInfo(MASTER_PID)).allocPoint, 0)) + Number(taskArgs.allocPoint)
+  tx = await MCv1.set(MASTER_PID, newAlloc)
+  await tx.wait();
 
-    console.log("Setting new MCv2 pool allocation...")
-    let pid = (await MCv2.poolInfoAmount) - 1
-    tx = await MCv2.set(pid, allocPoint, rewarders, overwrite, taskArgs.update)
-    await tx.wait();
+  console.log("Setting new MCv2 pool allocation...")
+  let pid = (await MCv2.poolInfoAmount) - 1
+  tx = await MCv2.set(pid, allocPoint, rewarders, overwrite, taskArgs.update)
+  await tx.wait();
 });
 
 // This is a sample Hardhat task. To learn how to create your own go to
@@ -60,30 +61,36 @@ task("addpool", "Adds pool to MCv2").addParam("allocPoint", "Amount of points to
 
 const getAccounts = (network: string) => {
   const accounts = {
+    goerli: process.env.PRIVATE_KEY ? process.env.PRIVATE_KEY : [],
     polygon: process.env.PRIVATE_KEY ? [process.env.PRIVATE_KEY] : [],
-    stagenet: process.env.STAGENET_PRIVATE_KEY ? [process.env.STAGENET_PRIVATE_KEY] : [],
   };
-
   if (!accounts[network]) {
     throw new Error(`Your account environment variables for ${network} are not set`);
   }
-
   return accounts[network];
 };
 /**
  * @type import('hardhat/config').HardhatUserConfig
  */
 const config: HardhatUserConfig = {
-  defaultNetwork: "hardhat",
   mocha: {
-    timeout: 20000,
+    timeout: 40000,
   },
   etherscan: {
     apiKey: process.env.API_KEY
   },
+  defaultNetwork: "goerli",
   networks: {
+    hardhat: {
+    },
+    goerli: {
+      url: process.env.GOERLI_URL,
+      accounts: getAccounts("goerli"),
+      chainId: 5,
+      gasPrice: 45000000000,
+    },
     polygon: {
-      url: process.env.INFURA_URL,
+      url: process.env.GOERLI_URL,
       accounts: getAccounts("polygon"),
       chainId: 137,
       gasPrice: 45000000000,
@@ -97,7 +104,7 @@ const config: HardhatUserConfig = {
   solidity: {
     compilers: [
       {
-        version: "0.8.10",
+        version: "0.8.17",
         settings: {
           optimizer: {
             enabled: true,
